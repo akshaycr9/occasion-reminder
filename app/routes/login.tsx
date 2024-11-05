e9bc5d88ac5aea079ef7ba37d4d3079aff030e7d
Akshay Thadani <akshay.thadani@gmail.com>
2024-11-05 23:20:13 +0530

    Added styles for login and verify page

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -1,9 +1,25 @@
-// app/routes/login.tsx
-import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
-import { json } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, json, LoaderFunctionArgs } from "@remix-run/node";
+import { Form, MetaFunction, useNavigation } from "@remix-run/react";
+import { Loader2 } from "lucide-react";
+
+import { Button } from "~/components/ui/button";
+import { Input } from "~/components/ui/input";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
 import { authenticator } from "~/modules/auth/auth.server";
-import { getSession, commitSession } from "~/modules/auth/session.server";
+import { commitSession, getSession } from "~/modules/auth/session.server";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Login - Occasion Reminder" },
+    { name: "description", content: "Login to Occasion Reminder" },
+  ];
+};
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await authenticator.isAuthenticated(request, {
@@ -23,7 +39,7 @@ export async function loader({ request }: LoaderFunctionArgs) {
   );
 }
 
-export async function action({ request }: ActionFunctionArgs) {
+export const action = async ({ request }: ActionFunctionArgs) => {
   await authenticator.authenticate("TOTP", request, {
     // The `successRedirect` route will be used to verify the OTP code.
     // This could be the current pathname or any other route that renders the verification form.
@@ -33,27 +49,43 @@ export async function action({ request }: ActionFunctionArgs) {
     // This could be the current pathname or any other route that renders the login form.
     failureRedirect: "/login",
   });
-}
+};
 
-export default function Login() {
-  let { authError } = useLoaderData<typeof loader>();
+export default function LoginForm() {
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === "submitting";
 
   return (
-    <div className="flex flex-col items-center">
-      {/* Login Form. */}
-      <Form method="POST">
-        <label htmlFor="email">Email</label>
-        <input
-          type="email"
-          name="email"
-          placeholder="Insert email .."
-          required
-        />
-        <button type="submit">Send Code</button>
-      </Form>
-
-      {/* Login Errors Handling. */}
-      <span>{authError?.message}</span>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">Login</CardTitle>
+          <CardDescription>
+            Enter your email to receive a verification code.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form method="post">
+            <div className="space-y-6">
+              <Input
+                type="email"
+                name="email"
+                placeholder="Please enter your email"
+              />
+              <Button type="submit" className="w-full" disabled={isSubmitting}>
+                {isSubmitting ? (
+                  <>
+                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                    Sending Code
+                  </>
+                ) : (
+                  "Send Verification Code"
+                )}
+              </Button>
+            </div>
+          </Form>
+        </CardContent>
+      </Card>
     </div>
   );
 }
--- app/routes/verify.tsx
+++ app/routes/verify.tsx
@@ -1,9 +1,29 @@
-// app/routes/verify.tsx
 import type { LoaderFunctionArgs, ActionFunctionArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useLoaderData } from "@remix-run/react";
+import { Form, MetaFunction, useLoaderData } from "@remix-run/react";
 import { authenticator } from "~/modules/auth/auth.server";
 import { getSession, commitSession } from "~/modules/auth/session.server";
+import {
+  InputOTP,
+  InputOTPGroup,
+  InputOTPSlot,
+} from "~/components/ui/input-otp";
+import { REGEXP_ONLY_DIGITS_AND_CHARS } from "input-otp";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "~/components/ui/card";
+import { Button } from "~/components/ui/button";
+
+export const meta: MetaFunction = () => {
+  return [
+    { title: "Verify OTP - Occasion Reminder" },
+    { name: "description", content: "Verify OTP" },
+  ];
+};
 
 export async function loader({ request }: LoaderFunctionArgs) {
   await authenticator.isAuthenticated(request, {
@@ -40,22 +60,67 @@ export default function Verify() {
   const { authError } = useLoaderData<typeof loader>();
 
   return (
-    <div className="flex flex-col items-center">
-      {/* Code Verification Form */}
-      <Form method="POST">
-        <label htmlFor="code">Code</label>
-        <input type="text" name="code" placeholder="Insert code .." required />
-        <button type="submit">Continue</button>
-      </Form>
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <Card className="w-full max-w-md">
+        <CardHeader>
+          <CardTitle className="text-2xl font-bold">Verify OTP</CardTitle>
+          <CardDescription>
+            Enter the 6-digit code sent to your email.
+          </CardDescription>
+        </CardHeader>
+        <CardContent className="flex flex-col gap-4">
+          <Form method="POST">
+            <div className="flex flex-col gap-4">
+              <InputOTP
+                name="code"
+                maxLength={6}
+                pattern={REGEXP_ONLY_DIGITS_AND_CHARS}
+                autoFocus
+                required
+              >
+                <InputOTPGroup className="flex justify-between w-full gap-1">
+                  <InputOTPSlot
+                    className="w-full h-12 p-3 text-lg text-center border rounded-md outline-none border-gray-300 focus:ring-2 focus:ring-blue-500"
+                    index={0}
+                  />
+                  <InputOTPSlot
+                    className="w-full h-12 p-3 text-lg text-center border rounded-md outline-none border-gray-300 focus:ring-2 focus:ring-blue-500"
+                    index={1}
+                  />
+                  <InputOTPSlot
+                    className="w-full h-12 p-3 text-lg text-center border rounded-md outline-none border-gray-300 focus:ring-2 focus:ring-blue-500"
+                    index={2}
+                  />
+                  <InputOTPSlot
+                    className="w-full h-12 p-3 text-lg text-center border rounded-md outline-none border-gray-300 focus:ring-2 focus:ring-blue-500"
+                    index={3}
+                  />
+                  <InputOTPSlot
+                    className="w-full h-12 p-3 text-lg text-center border rounded-md outline-none border-gray-300 focus:ring-2 focus:ring-blue-500"
+                    index={4}
+                  />
+                  <InputOTPSlot
+                    className="w-full h-12 p-3 text-lg text-center border rounded-md outline-none border-gray-300 focus:ring-2 focus:ring-blue-500"
+                    index={5}
+                  />
+                </InputOTPGroup>
+              </InputOTP>
 
-      {/* Renders the form that requests a new code. */}
-      {/* Email input is not required, it's already stored in Session. */}
-      <Form method="POST">
-        <button type="submit">Request new Code</button>
-      </Form>
-
-      {/* Errors Handling. */}
-      <span>{authError?.message}</span>
+              <div className="flex flex-col space-y-2">
+                <Button type="submit" className="w-full">
+                  Verify OTP
+                </Button>
+              </div>
+            </div>
+          </Form>
+          <Form method="POST">
+            <Button type="submit" variant="outline" className="w-full">
+              Resend Code
+            </Button>
+          </Form>
+        </CardContent>
+        <span className="text-center text-red-700">{authError?.message}</span>
+      </Card>
     </div>
   );
 }
