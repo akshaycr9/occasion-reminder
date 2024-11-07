b49f4cb161665167a06f09033fcd5455b5f0bd6f
Akshay Thadani <akshay.thadani@gmail.com>
2024-11-07 12:37:38 +0530

    Restructured routes configuration

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,21 @@ export default defineConfig({
   plugins: [
     remixDevTools(),
     remix({
+      routes(defineRoutes) {
+        return defineRoutes((route) => {
+          route("/", "home.tsx", { index: true });
+          route("login", "auth/login.tsx");
+          route("verify", "auth/verify.tsx");
+          route("logout", "auth/logout.tsx");
+          route("magic-link", "auth/magic-link.tsx");
+          route("/users", "users/route.tsx", () => {
+            route("", "users/index.tsx", { index: true });
+            route(":userId", "users/user.tsx");
+            route(":userId/edit", "users/edit.tsx");
+            route("create", "users/create.tsx");
+          });
+        });
+      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
